d0a94d700c88d0b6a004ef6d7ea5efecd236af0e
noob-anmol <anmolghai50@gmail.com>
2024-03-20 00:52:07 +0530

    started implementing crossword generation

--- frontend/src/components/Crossword/Crossword.jsx
+++ frontend/src/components/Crossword/Crossword.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "./Crossword.css"
+import "./Crossword.css";
 const Crossword = () => {
-
 	return (
-		<div className="crossword-container">
+		<div className='crossword-container'>
 			<div className='row'>
 				<div className='column'></div>
 				<div className='column'></div>
